0bc2b407c445b9a1c5e8173b80c0bfdffcf37d98
Vitalijus <lyja.vp@gmail.com>
2025-02-18 11:20:21 +0200

    Finished

--- BonusTask/src/Main.java
+++ BonusTask/src/Main.java
@@ -1,5 +1,32 @@
 import java.util.Scanner;
 public class Main {
-    Scanner scanner = new Scanner(System.in);
-    String s = scanner.next();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = scanner.nextLine();
+        System.out.println("Pradinė eilutė: " + line);
+
+        if(line.isEmpty()){
+            System.out.println("Atsakymas: " + stringBuilder);
+        }
+
+        char left = line.charAt(0), right = ' ';
+
+        int count = 1;
+        for(int i = 1; i < line.length(); i++){
+            right = line.charAt(i);
+            if(left != right){
+                stringBuilder.append(left);
+                stringBuilder.append(count);
+                count = 1;
+                left=right;
+                continue;
+            }
+            count++;
+        }
+        stringBuilder.append(left);
+        stringBuilder.append(count);
+
+        System.out.println("Atsakymas: " + stringBuilder);
+    }
 }
